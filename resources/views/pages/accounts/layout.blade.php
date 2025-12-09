c939126e3de62223d4d5618990ed989e4cf5031a
Hailinh1509 <linhham1509@gmail.com>
2025-12-10 01:16:23 +0700

    Change footer layout

--- resources/views/pages/accounts/layout.blade.php
+++ resources/views/pages/accounts/layout.blade.php
@@ -69,7 +69,7 @@
     </div>
   </div>
 
-  @include('partials.footer')
+  @include('footer.footer')
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
   @yield('scripts')
